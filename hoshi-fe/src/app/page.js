1305ea791a75ae62719df74f913dad7b937d88c1
rik <vslimrik@gmail.com>
2024-10-18 21:57:09 -0700

    add topbar

--- hoshi-fe/src/app/page.js
+++ hoshi-fe/src/app/page.js
@@ -1,6 +1,9 @@
 'use client';
 import { useRef } from 'react';
 import { useVideo } from './providers/VideoProvider';
+import hoshiLogoLight from './media/logo/hoshi-logo-light.png';
+import { Flame, MessageCircle } from 'lucide-react';
+import Image from 'next/image';
 
 export default function Home() {
   const openingVideo = '../media/opening-vid-hoshi.mp4';
@@ -27,6 +30,26 @@ export default function Home() {
           Your browser does not support the video tag.
         </video>
       )}
+
+      <div className='relative min-h-screen flex flex-col bg-[#1C1C1E] text-white'>
+        <header className='z-50 fixed top-0 inset-x-0 h-16 flex justify-center items-center p-4 border-b border-gray-700 bg-[#1C1C1E]'>
+          <Image
+            src={hoshiLogoLight}
+            alt='Hoshi logo'
+            width={100}
+            height={140}
+            unoptimized
+          />
+          <div className='absolute right-4 flex flex-row gap-1'>
+            <button className='border border-gray-700 rounded-lg p-2 hover:bg-gray-700 hover:border-gray-600'>
+              <Flame size={20} color='#fff4d1' />
+            </button>
+            <button className='border border-gray-700 rounded-lg p-2 hover:bg-gray-700 hover:border-gray-600'>
+              <MessageCircle size={20} color='#fff4d1' />
+            </button>
+          </div>
+        </header>
+      </div>
     </div>
   );
 }
